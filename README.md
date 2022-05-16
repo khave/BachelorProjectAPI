bdbcd022dbd757195f89e38ef7ae3851ea2b46be
khave <kristian.have@hotmail.com>
2022-05-16 15:01:18 +0200

    Added Postman file & fixed JSON response error

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BachelorProjectAPI
 API to predict LEGO sets based on images
+
+Use 
+curl --location --request POST 'http://localhost:5000/predict' \
+--form 'img=@"/Z:/SDU/6. Semester/Bachelor Projekt/Datasets/dataset 224x224 partially-built-big [ALL sets]/test/76902/frame_87.jpg"'
+
+to predict an image or Postman.
--- flask_api.py
+++ flask_api.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import numpy as np
 import mlflow as mlflow
 import cv2
 import base64
+import json
 
 app = Flask(__name__)
 
@@ -14,24 +15,35 @@ print("Model loaded!")
 labels = ['21034', '31058', '31088', '31112', '31113', '31114', '31121', '40220', '40468', '40469', '40532', '42134', '75280', '75297', '75299', '76901', '76902']
 
 
+class NumpyFloatValuesEncoder(json.JSONEncoder):
+    '''
+        Class to encode numpy float32 values to json
+    '''
+    def default(self, obj):
+        if isinstance(obj, np.float32):
+            return float(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
     print("Making prediction...")
     data = request.files["img"]
     img = cv2.imdecode(np.fromstring(data.read(), np.uint8), cv2.IMREAD_UNCHANGED)
     img = cv2.resize(img, (224, 224))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    img = img.astype(np.float32)
     img = np.expand_dims(img, axis=0)
     pred = model.predict(img)
-    print(f"Raw prediction: {pred}")
+    
     # Format the prediction as a JSON object with the label and probability of the predicted classes
     pred_dict = {}
     for i in range(len(labels)):
         pred_dict[labels[i]] = pred[0][i]
     
-    print("Pred dict:", pred_dict)
-
     print(f"Prediction made and got label: {labels[np.argmax(pred)]}")
-    return jsonify(pred_dict)
+    return json.dumps(pred_dict, cls=NumpyFloatValuesEncoder)
 
 
 
